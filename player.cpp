2dc3e6419e80be5ee57f29252e7b21dea3bdfbbf
kam0lia <kamalrashad56@gnail.com>
2026-05-10 07:59:53 +0300

    player class done

---
+++ player.cpp
@@ -0,0 +1,44 @@
+#include"player.h"
+Player::Player()
+{
+    name = "";
+    symbol = ' ';
+    score = 0;
+}
+Player::Player(std::string name, char symbol)
+{
+    this->name = name;
+    this->symbol = symbol;
+    score = 0;
+}
+std::string Player::getName()
+{
+    return name;
+}
+char Player::getSymbol()
+{
+    return symbol;
+}
+int Player::getScore()
+{
+    return score;
+}
+void Player::incrementScore()
+{
+    score++;
+}
+void Player::setName(std::string name)
+{
+    this->name = name;
+}
+void Player::setSymbol(char symbol)
+{
+    this->symbol = symbol;
+}
+void Player::validateSymbol(char symbol)
+{
+    if(symbol != 'X' && symbol != 'O')
+    {
+        throw std::invalid_argument("Invalid symbol. Symbol must be 'X' or 'O'.");
+    }
+}
---
+++ player.h
@@ -0,0 +1,22 @@
+#ifndef player_h
+#define player_h
+#include<iostream>
+#include<string>
+class Player
+{  
+    private:
+    std::string name;
+    char symbol;
+    int score;
+    public:
+    Player();
+    Player(std::string name, char symbol);
+    std::string getName();
+    char getSymbol();
+    int getScore();
+    void incrementScore();
+    void setName(std::string name);
+    void setSymbol(char symbol);
+    void validateSymbol(char symbol);
+ };
+#endif 
